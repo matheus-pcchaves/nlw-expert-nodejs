04fa61e099393f46f21fcfed6a2e8c4a90771706
matheus-pcchaves <math.cchaves@gmail.com>
2024-02-09 19:48:18 -0300

    rotas da aplicação

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,9 +1,20 @@
 import fastify from 'fastify'
+import cookie from '@fastify/cookie'
 import { createPoll } from '../routes/create-poll';
+import { getPoll } from '../routes/get-poll';
+import { voteOnPoll } from '../routes/vote-on-poll';
 
 const app = fastify()
 
+app.register(cookie, {
+    secret: "nlw-expert-node",
+    hook: 'onRequest',
+    parseOptions: {}
+})
+
 app.register(createPoll)
+app.register(getPoll)
+app.register(voteOnPoll)
 
 app.listen({ port:3333}).then(() => {
     console.log('Server is running')
